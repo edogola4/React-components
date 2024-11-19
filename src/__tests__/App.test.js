3a0fe939f0054570bb58afb9f950183b21d443d9
Allan Ochieng <alnothigo@gmail.com>
2024-11-19 08:00:35 +0300

    Initial files upload
    files upload

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,19 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import App from "../components/App";
+
+test("renders without errors", () => {
+  expect(() => render(<App />)).not.toThrow();
+});
+
+test("renders the correct child components", () => {
+  const { container } = render(<App />);
+
+
+  screen.debug();
+
+  expect(container.querySelector("nav")).toBeInTheDocument();
+  expect(container.querySelector("#home")).toBeInTheDocument();
+  expect(container.querySelector("#about")).toBeInTheDocument();
+});
